c7fcb2bea6bac9dd45c42e314e1a3090cd8fcf77
Shabeeb Qadir <sqadir@zynga.com>
2012-11-21 18:33:59 +0530

    Fixing Indentation.

--- dm_server/lib/urlmapper.py
+++ dm_server/lib/urlmapper.py
@@ -24,7 +24,7 @@ def index(environ, start_response):
         action = query_string["action"]
         if "get_host_config" in action:
             return dm.get_host_config()
-		elif "get_all_config" in action:
+        elif "get_all_config" in action:
             return dm.get_all_config()
 
     return dm.forward_request()
