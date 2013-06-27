083eb75d5df3bf1c00314f6f770b0e3ef1a85a72
Sarath Lakshman <slakshman@zynga.com>
2013-06-27 12:45:52 +0530

    Add license

--- build_dev_rpms.sh
+++ build_dev_rpms.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#   Copyright 2013 Zynga Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 dm_server="netops-demo-mb-210.va2.zynga.com"
 ss_server="netops-demo-mb-211.va2.zynga.com,netops-demo-mb-212.va2.zynga.com,netops-demo-mb-213.va2.zynga.com"
 #dm_server="10.36.172.154"
