13d74032f4251bc74f7cd6683c12af2df9b5f884
Sarath Lakshman <slakshman@zynga.com>
2013-08-13 14:23:52 +0530

    Update license

--- storage_server/backup_purger.sh
+++ storage_server/backup_purger.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 #
+#   Copyright 2013 Zynga Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
 # This script deletes old master and daily backups.
 #
 # This script is to be run on the storage servers.
@@ -15,7 +30,6 @@
 #
 #   -t Test run : Will only display files to be deleted won't delete the files.	
 #
-# Copyright 2010 Zynga, Inc. All rights reserved
 
 AUTHOR='Shabeeb Qadir <sqadir@zynga.com>'
 
